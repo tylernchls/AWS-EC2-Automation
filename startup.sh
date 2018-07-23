14de33becaa202f40796a5a68b57bdb2ae727444
tylernchls <tylernchls@gmail.com>
2018-07-22 22:28:16 -1000

    updates script

--- startup.sh
+++ startup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 echo "Please wait while I build your infrastructure for you."
-sleep 3
-echo "Doing some cool stuff in the background"
 node app.js
 sleep 3
 echo "Thanks for waiting, All done here. Have a nice day :)"
